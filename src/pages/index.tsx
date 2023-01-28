60a35012293b7eec53ada05443efcaa63a639449
Joshua Kappers <jkappers@concordservicing.com>
2023-01-28 15:40:39 -0600

    Select boxes working before async metro load

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,113 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import { useReducer, useCallback } from 'react';
+import Select, { ActionMeta, Props as SelectProps } from 'react-select'
 
-const inter = Inter({ subsets: ['latin'] })
+const states = [
+  { value: 0, label: 'Texas' },
+  { value: 1, label: 'Louisiana' }
+]
+
+const allMetros = [
+  [
+    { value: 0, label: "Dallas" },
+    { value: 1, label: "Houston" }
+  ],
+  [
+    { value: 0, label: "Bossier" },
+    { value: 1, label: "Haughton" }
+  ]
+]
+
+const StateSelect = (props: SelectProps) => (
+  <Select {...props} isClearable />
+)
+
+const MetroSelect = (props: SelectProps) => (
+  <Select {...props} isMulti isClearable isDisabled={!props.options?.length} />
+)
+
+interface Option
+{
+  value: number,
+  label: string
+}
+
+interface FilterState
+{
+  states: Option[],
+  metros?: Option[],
+  selectedState?: Option | null,
+  selectedMetros?: Option[]
+}
+
+interface FilterStateAction
+{
+  type: FilterStateActionKind,
+  payload?: any
+}
+
+enum FilterStateActionKind
+{
+  CHANGE_STATE,
+  CHANGE_METRO,
+  RESET
+}
+
+const getMetroOptions = (stateId?: number) : Option[] => {
+  if (!stateId?.toString) {
+    return []
+  }
+
+  return allMetros[stateId];  
+}
+
+const initialState: FilterState = {
+  states,
+  metros: getMetroOptions(states[0].value),
+  selectedState: states[0],
+  selectedMetros: []
+}
+
+const reducer = (state: FilterState, action: FilterStateAction): FilterState => {
+  console.log(action.type)
+  switch (action.type) {
+    case FilterStateActionKind.CHANGE_STATE:
+      const nextState = { ...state, selectedState: action.payload, selectedMetros: [] }
+
+      nextState.metros = getMetroOptions(nextState.selectedState?.value);
+
+      return nextState;
+    case FilterStateActionKind.CHANGE_METRO:
+      return { ...state, selectedMetros: action.payload };
+    case FilterStateActionKind.RESET:
+      return { ...state, selectedState: null, selectedMetros: [] }
+    default:
+      return state;
+  }
+};
 
 export default function Home() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+ 
+  const onStateSelectChanged = useCallback(
+    (newValue: unknown, _actionMeta: ActionMeta<unknown>): void =>
+      dispatch({ type: FilterStateActionKind.CHANGE_STATE, payload: newValue })
+    , []
+  );
+
+  const onMetroSelectChanged = useCallback(
+    (newValue: unknown, _actionMeta: ActionMeta<unknown>): void =>
+      dispatch({ type: FilterStateActionKind.CHANGE_METRO, payload: newValue })
+    , []
+  );
+
+  const onResetButtonClicked = useCallback(
+    (e: React.MouseEvent<HTMLButtonElement>): void => {
+      dispatch({ type: FilterStateActionKind.RESET })
+    }, []
+  )
+
   return (
     <>
       <Head>
@@ -14,109 +116,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+      <main>
+        <StateSelect options={states} onChange={onStateSelectChanged} value={state.selectedState} />
+        <MetroSelect options={state.metros} isMulti onChange={onMetroSelectChanged} value={state.selectedMetros} />
+        <button onClick={onResetButtonClicked}>Reset</button>
       </main>
     </>
   )
